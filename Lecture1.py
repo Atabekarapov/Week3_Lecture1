acf8e6e40dc93c8dfed469e50d23017f16ca4d8e
Atabek <atabekarapov14@gmail.com>
2020-11-02 12:41:03 +0600

    Week3_Lecture1

---
+++ Lecture1.py
@@ -0,0 +1,135 @@
+# NONETYPE    -----> tip dannyh,
+# BOOL (TRUE OR FALSE) ---->  tip dannyh, 
+# a = input('')
+# print(type(a))
+
+# 1)
+# a = '' and ''           #      doljny poluchit oba usloviya, dva usloviya doljny byt pravym
+
+# b = '' or ''              #     odno usloviya doljen byt pravym
+
+# a = not True       #     
+# a = 3 + 5
+# c = 2 + 3
+# b = c is a
+# print(bool(b))
+# print(id(a))
+# print(id(c))
+# print(bool(b))
+
+# 2)
+# list_ = (0,)                 #esli budet pustoi to TRUE 
+# print(bool(list_))  
+
+# 3) 
+# a = 2 < 1 and['']
+# b = '' or (0,)
+# a = 2 <= 5 or not b
+
+# print(bool(a))
+
+# Uslovnye operatory
+# 4)
+# if condition:
+#     print('Blue')
+# elif condition:
+#     print('white')
+# elif condition:
+#     print('Green')
+# else:
+#     print('Red')
+
+# 5)
+# if 7 > 5:
+#     print(True)
+# else:
+#     print(False)
+
+# 6)
+# students = []
+# if not students:
+#     print('skfjkdjk')
+# else:
+#     print('makers')
+
+# 7)
+# grade = int(input('Enter your mark for today: '))
+# if grade.isdigit():
+#     if grade == 5:
+#         print('Wow you did a great job!')
+#     elif grade == 4:
+#         print('This is okay buddy:)')
+#     elif grade == 3:
+#         print('You have to work on that!!!')
+#     else: 
+#         print("WTF work hard")
+# else:
+#     print('Enter the number please')
+
+
+#  8)
+# age = int(input('Enter your age: '))
+
+# if age > 18:
+#     print('From you 200som!')
+# elif age > 7:
+#     print('From you 50som!')
+# else:
+#     print('Just come to watch the movie:)')
+# print('OK')
+
+# 9)
+s = input('Say what you want: ')
+res = 'Hi, cat' if s == 'Meawwww' or s == 'awaw' else 'Who are you?'
+print(res)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
